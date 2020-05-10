c046d623ee828e3deb82c537f404321eea3691fd
Beatmeda <beatmeda@gmail.com>
2020-05-09 20:44:39 -0500

    Se sube proyecto E-Commerce spree.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,4 @@
+class ApplicationController < ActionController::Base
+  SpreeI18n::Config.available_locales = [:en, :es, :'pt-BR'] # displayed on frontend select box
+  SpreeGlobalize::Config.supported_locales = [:en, :es, :'pt-BR'] # displayed on translation forms
+end
